d1a6292d68495ed26ede1c8a98b183658afa45d5
ThisalDinuwara <tkandepola@gmail.com>
2025-11-01 10:06:59 +0530

    First commit

---
+++ ecommerce-web/src/main/java/com/digitalworld/ecommerce/web/EcommerceWebApplication.java
@@ -0,0 +1,13 @@
+package com.digitalworld.ecommerce.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebApplication.class, args);
+	}
+
+}
