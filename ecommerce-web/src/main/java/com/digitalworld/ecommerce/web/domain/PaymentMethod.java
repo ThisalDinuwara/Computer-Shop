efced3ef0e84a9bdb3a72f669f42695a5dc3445e
ThisalDinuwara <tkandepola@gmail.com>
2025-11-02 14:54:58 +0530

    version 0.0.8

---
+++ ecommerce-web/src/main/java/com/digitalworld/ecommerce/web/domain/PaymentMethod.java
@@ -0,0 +1,6 @@
+package com.digitalworld.ecommerce.web.domain;
+
+public enum PaymentMethod {
+    RAZORPAY,
+    STRIPE
+}
---
+++ ecommerce-web/src/main/java/com/digitalworld/ecommerce/web/domain/PaymentOrderStatus.java
@@ -0,0 +1,7 @@
+package com.digitalworld.ecommerce.web.domain;
+
+public enum PaymentOrderStatus {
+    PENDING,
+    SUCCESS,
+    FAILED
+}
